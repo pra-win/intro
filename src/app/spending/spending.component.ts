eec1c7bedd50d3ca459a59330ec4f904c34ce17b
pravin <bhosale.pravinp@gmail.com>
2019-08-10 17:38:52 +0530

    spendings

--- src/app/spending/spending.component.ts
+++ src/app/spending/spending.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TransactionsService } from './../services/transactions.service';
 
 @Component({
   selector: 'app-spending',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpendingComponent implements OnInit {
 
-  constructor() { }
+  transactionsData = [];
+  income = [];
+  expense = [];
+  incomeTotal = 0;
+  expenseTotal = 0;
+
+  constructor(private transactions: TransactionsService) { }
 
   ngOnInit() {
+    this.transactions.getTransactions().subscribe((data) => {
+      this.transactionsData = data.response;
+      if(data.response) {
+        this.setIncomeExpence(data.response);
+      }
+    });
+  }
+
+  setIncomeExpence(transactionsData) {
+    transactionsData.forEach((t)=>{
+      if(t.ctype === 'e') {
+        this.expense.push(t);
+        this.expenseTotal+=t.amt;
+      } else {
+        this.income.push(t);
+        this.incomeTotal+=t.amt;
+      }
+    });
   }
 
 }
