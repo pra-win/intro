cfa104b29d25d80d82fa7738668365e071aa3e71
pravin <bhosale.pravinp@gmail.com>
2019-08-17 13:10:42 +0530

    loading changes

--- src/app/loadingspinner/loadingspinner.component.ts
+++ src/app/loadingspinner/loadingspinner.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { BsModalService, BsModalRef } from 'ngx-bootstrap';
 import { LoadingService } from './../services/loading.service';
 
 @Component({
@@ -8,36 +7,16 @@ import { LoadingService } from './../services/loading.service';
   styleUrls: ['./loadingspinner.component.css']
 })
 export class LoadingspinnerComponent implements OnInit {
-  modalRef: BsModalRef;
-  config = {
-    backdrop: true,
-    ignoreBackdropClick: true,
-    class: 'loading-spinner'
-  };
-
-    @ViewChild('template', {static: false}) input;
-
-  constructor(private modalService: BsModalService, private loading:LoadingService) { }
+  showLoading = false;
+  constructor(private loading:LoadingService) { }
 
   ngOnInit() {
     this.loading.getLoading().subscribe(data => {
-        if(data.loadingBar) {
-          console.log("show loading");
-          //this.modalRef = this.modalService.show(this.input, this.config);
-        } else {
-
-          setTimeout(() => {
-            console.log("hide loading");
-          //  this.modalRef.hide();
-          },1000);
-
-        }
+        this.showLoading = data.loadingBar;
+        console.log("show", this.showLoading);
     });
   }
 
-  ngAfterViewInit() {
-    //console.log(this.input);
-
-  }
+  ngAfterViewInit() {}
 
 }
