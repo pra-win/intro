8a26b5dbfacec09ea296decc04626d44e26f7ed0
pra-win <bhosale.pravinp@gmail.com>
2019-09-03 06:39:33 +0530

    validations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -28,9 +28,9 @@ export class HomeComponent implements OnInit {
     // });
 
     this.employeeForm = this.fb.group({
-      fullName: [''],
+      fullName: ['',[Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
       email: [''],
-      skills: this.fb.group({       
+      skills: this.fb.group({
         skillName: [''],
         experience: [''],
         proficiency: ['']
